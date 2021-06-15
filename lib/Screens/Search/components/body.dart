8ad064c7392c8edc6fda294438ae5309beb38acd
Dekaver <alwiwahyudi8@gmail.com>
2021-06-15 22:56:08 +0700

    fix

--- lib/Screens/Search/components/body.dart
+++ lib/Screens/Search/components/body.dart
@@ -1,7 +1,12 @@
 import 'package:flappy_search_bar/flappy_search_bar.dart';
 import 'package:flutter/material.dart';
 
-class Body extends StatelessWidget {
+class Body extends StatefulWidget {
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
   Future<List<Post>> search(String search) async {
     await Future.delayed(Duration(seconds: 2));
     return List.generate(search.length, (int index) {
@@ -11,23 +16,42 @@ class Body extends StatelessWidget {
       );
     });
   }
+  List<String> _locations = ['A', 'B', 'C', 'D'];
+  String _selectedLocation;
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: SearchBar<Post>(
-          onSearch: search,
-          onItemFound: (Post post, int index) {
-            return ListTile(
-              title: Text(post.title),
-              subtitle: Text(post.deskripsi),
-            );
-          },
-        ),
-      )
-    );
+    return Center(
+          child: DropdownButton(
+            hint: Text('Please choose a location'), // Not necessary for Option 1
+            value: _locations,
+            onChanged: (newValue) {
+              setState(() {
+                _selectedLocation = newValue;
+              });
+            },
+            items: _locations.map((location) {
+              return DropdownMenuItem(
+                child: new Text(location),
+                value: location,
+              );
+            }).toList(),
+          ),
+        );
+    // return SafeArea(
+    //   child: Padding(
+    //     padding: const EdgeInsets.symmetric(horizontal: 20),
+    //     child: SearchBar<Post>(
+    //       onSearch: search,
+    //       onItemFound: (Post post, int index) {
+    //         return ListTile(
+    //           title: Text(post.title),
+    //           subtitle: Text(post.deskripsi),
+    //         );
+    //       },
+    //     ),
+    //   )
+    // );
   }
 }
 
--- lib/Screens/Welcome/Components/body.dart
+++ lib/Screens/Welcome/Components/body.dart
@@ -5,66 +5,74 @@ import 'package:homely/Screens/Welcome/Components/background.dart';
 import 'package:homely/components/rounded_button.dart';
 import 'package:homely/constants.dart';
 
-
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
     return Background(
-      child:SingleChildScrollView(
-        child: Column(
-          
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            
-            Text(
-              "WELCOME TO HOMELY",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: size.height * 0.05),
-            Image.asset(
-              'assets/images/house.png',
-              height: size.height * 0.45,
-            ),
-            SizedBox(height: size.height * 0.05),
-
-            RoundedButton(  
-              text: "login",
-              color: Colors.deepPurple.shade200,
-              textColor: Colors.white,
-              background: MaterialStateProperty.all<Color>(Colors.deepPurple.shade200),
-              rounded: 18.0,
-              press: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return LoginScreen();
-                    },
-                  ),
-                );
-              },
-            ),
-            RoundedButton(
-              text: "sign up",
-              color: aPrimaryLightColor,
-              textColor: Colors.deepPurple.shade200,
-              rounded: 18.0,
-              press: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return SignUpScreen();
-                    },
-                  ),
-                );
-              },
-            ),
-          ],
-        ),
-      ) 
-    );
+        child: SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "WELCOME TO HOMELY",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: size.height * 0.05),
+          Image.asset(
+            'assets/images/house.png',
+            height: size.height * 0.45,
+          ),
+          SizedBox(height: size.height * 0.05),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 10),
+            width: size.width * 0.8,
+            child: TextButton(
+                key: Key('buttonKeyLogin'),
+                child:
+                    Text("login".toUpperCase(), style: TextStyle(fontSize: 14)),
+                style: ButtonStyle(
+                    padding: MaterialStateProperty.all<EdgeInsets>(
+                        EdgeInsets.all(15)),
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        Colors.deepPurple.shade200),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0),
+                            side: BorderSide(
+                                color: Colors.deepPurple.shade200)))),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return LoginScreen();
+                      },
+                    ),
+                  );
+                }),
+          ),
+          RoundedButton(
+            text: "sign up",
+            color: aPrimaryLightColor,
+            textColor: Colors.deepPurple.shade200,
+            rounded: 18.0,
+            press: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return SignUpScreen();
+                  },
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    ));
   }
 }
